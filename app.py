c735ef8d9b380b629fbdbc0624e1d36b47fee10c
karthikcd7 <karthikcd7@gmail.com>
2020-05-29 00:34:29 +0530

    Update

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/gallery")
+@app.route("/home/gallery")
 def gallery():
     return render_template("gallery.html")
 
-@app.route("/events")
+@app.route("/home/events")
 def events():
     return render_template("events.html")
 
-@app.route("/events/events01")
+@app.route("/home/events/events01")
 def events01():
     return render_template("events01.html")
 
-@app.route("/events/events02")
+@app.route("/home/events/events02")
 def events02():
     return render_template("events02.html")
 
-@app.route("/events/events03")
+@app.route("/home/events/events03")
 def events03():
     return render_template("events03.html")
 
